deb8de9258ab7668b6d98b0771cab5d6c45441ce
Carlos Rodrigo <rodrigolimac137@gmail.com>
2022-03-07 09:18:16 -0300

    FEAT: finalize rent Added

--- src/app.js
+++ src/app.js
@@ -221,18 +221,15 @@ app.get("/rentals", async (req, res) => {
                 delayFee, customerName, gameName, categoryId, categoryName
             ] = row;
 
-            const initialDate = convert(Date(rentDate));
+            const initialDate = rentDate.toISOString().split("T")[0];
             let returned = null;
-            let delay = null;
             if(returnDate !== null){
-                returned = convert(Date(returnDate));
-            } else if( delayFee !== null){
-                delay = convert(Date(delayFee));
+                returned = returnDate.toISOString().split("T")[0];
             }
 
             return {
                 id, customerId, gameId, rentDate: initialDate, daysRented, 
-                returnDate: returned, originalPrice, delayFee: delay,
+                returnDate: returned, originalPrice, delayFee,
                 customer: { 
                     id: customerId, name: customerName
                 },
@@ -247,13 +244,6 @@ app.get("/rentals", async (req, res) => {
         res.sendStatus(500);
     }
 });
-
-function convert(str) {
-    var date = new Date(str),
-        mnth = ("0" + (date.getMonth() + 1)).slice(-2),
-        day = ("0" + date.getDate()).slice(-2);
-    return [date.getFullYear(), mnth, day].join("-");
-}
 
 app.post("/rentals", async (req, res) => {
     const {customerId, gameId, daysRented} = req.body;
@@ -309,39 +299,38 @@ app.post("/rentals", async (req, res) => {
 
 app.post("/rentals/:id/return", async (req, res) => {
     const {id} = req.params;
-    let priceDelay = 0;
+    let fee = 0;
     try {
         const existentRent = await connection.query(`
             SELECT * FROM rentals WHERE id = $1;
         `, [id]);
-        const existentGame = await connection.query(`
-            SELECT * FROM games WHERE id = $1;
-        `, [existentRent.rows[0].gameId]);
         if(existentRent.rows.length === 0){
             res.sendStatus(404);
         } else if(existentRent.rows[0].returnDate !== null){
             res.sendStatus(400);
         }
+        const existentGame = await connection.query(`
+            SELECT * FROM games WHERE id = $1;
+        `, [existentRent.rows[0].gameId]);
 
-        priceDelay = (
-            parseInt(existentRent.rendDate.split("-")[2])
-            + 
-            existentRent.daysRented
-            - 
-            parseInt(dayjs().format("DD"))
-            *
-            existentGame.rows[0].pricePerDay
-        );
+        const dateOfRent = parseInt(existentRent.rows[0].rentDate.toISOString().split("T")[0].split("-")[2]);
+        const numberOfDaysRented = existentRent.rows[0].daysRented;
+        const actualDay = parseInt(dayjs().format("DD"));
+        const priceAday = existentGame.rows[0].pricePerDay;
+
+        if((dateOfRent + numberOfDaysRented) < actualDay){
+            fee = (dateOfRent + numberOfDaysRented - actualDay) * priceAday;
+        };
 
 
         await connection.query(`
             UPDATE rentals SET "returnDate" = $1, "delayFee" = $2
             WHERE id = $3;
-        `, [dayjs().format("YYYY-MM-DD"), priceDelay, id]);
+        `, [dayjs().format("YYYY-MM-DD"), fee, id]);
 
-        res.sendStatus(500);
+        res.sendStatus(201);
     } catch (error) {
-        console.log(500);
+        console.log(error);
         res.sendStatus(500);
     }
 });
