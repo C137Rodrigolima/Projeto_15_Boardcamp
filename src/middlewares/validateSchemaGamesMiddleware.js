4067e0f8d5fbab4214593cc27b53aa888b88801a
Adriano <adrianogotardo.12@gmail.com>
2022-03-11 03:26:59 -0300

    Preparando review

--- src/middlewares/validateSchemaGamesMiddleware.js
+++
@@ -1,9 +0,0 @@
-export default function validateSchemaGameMiddleware(schema){
-    return (req, res, next) => {
-        const validation = schema.validate(req.body);
-        if(validation.error){
-            return res.sendStatus(400).send('Schema Inválido');
-        }
-        next();
-    }
-}
--- src/middlewares/validateSchemaRentalsMiddleware.js
+++
@@ -1,9 +0,0 @@
-export default function validateSchemaRentalsMiddleware(schema){
-    return (req, res, next) => {
-        const validation = schema.validate(req.body);
-        if(validation.error){
-            return res.sendStatus(400).send('Schema Inválido');
-        }
-        next();
-    }
-}
